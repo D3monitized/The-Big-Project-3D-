83de04b5f63d4b771ce9fb9633c82ad495d6eaca
Alexander Dolk <alexander@dolken.net>
2022-08-23 14:25:00 +0200

    House generation och gaia map

--- The Big Project (3D)/Assets/TerrainGen/Scripts/ChunkGenerator.cs
+++ The Big Project (3D)/Assets/TerrainGen/Scripts/ChunkGenerator.cs
@@ -6,7 +6,9 @@ public class ChunkGenerator : MonoBehaviour
 	[SerializeField]
 	private float ChunkRadius = 16;
 	[Range(1, 100)] [SerializeField]
-	private int VillageSpawnChance = 1; 
+	private int VillageSpawnChance = 1;
+	[Range(0, 90)] [SerializeField]
+	private int VillageMaxSlopeForSpawn = 45; 
 
 	[Header("Components")]
 	[SerializeField]
@@ -29,7 +31,8 @@ public class ChunkGenerator : MonoBehaviour
 			return; 
 
 		GetMapInfo(); 
-		GenerateChunks(); 
+		GenerateChunks();
+		PlaceChunkBuildings(); 
 	}
 
 	private void GetMapInfo()
@@ -59,13 +62,15 @@ public class ChunkGenerator : MonoBehaviour
 
 				if (currentPos.y <= WaterSurfaceTransform.position.y)
 					Chunks[x, y].type = Chunk.ChunkType.None;
+				else if (GetHighestSlopeAngle(currentPos, ChunkRadius) > VillageMaxSlopeForSpawn)
+					Chunks[x, y].type = Chunk.ChunkType.Woods;
 				else
 				{
 					float chance = VillageSpawnChance;
-					chance /= 100; 
-					float rand = Random.value;				
-					
-					Chunks[x, y].type = rand < chance ? Chunk.ChunkType.Village : Chunk.ChunkType.Woods; 
+					chance /= 100;
+					float rand = Random.value;
+
+					Chunks[x, y].type = rand < chance ? Chunk.ChunkType.Village : Chunk.ChunkType.Woods;
 				}
 
 				currentPos.x += ChunkRadius * 2; 
@@ -73,6 +78,21 @@ public class ChunkGenerator : MonoBehaviour
 
 			currentPos.z -= ChunkRadius * 2; 
 			currentPos.x = startPos.x; 
+		}
+	}
+
+	void PlaceChunkBuildings()
+	{
+		foreach(Chunk c in Chunks)
+		{
+			switch (c.type)
+			{
+				case Chunk.ChunkType.Village:
+					if (GetComponent<HouseGenerator2>())
+						GetComponent<HouseGenerator2>().GenerateHouses(c.Position, c.Radius);
+					break;
+				case Chunk.ChunkType.Woods: break;
+			}
 		}
 	}
 
@@ -87,6 +107,44 @@ public class ChunkGenerator : MonoBehaviour
 		}
 
 		return newPosition; 
+	}
+	
+	float GetHighestSlopeAngle(Vector3 Position, float ChunkRadius)
+	{
+		float currentMaxSlopeAngle = 0;
+
+		//Get positions of all directions
+		Vector3 eastSlope = Position + Vector3.right * ChunkRadius;
+		eastSlope = GetTerrainHeight(eastSlope); 
+		Vector3 westSlope = Position - Vector3.right * ChunkRadius;
+		westSlope = GetTerrainHeight(westSlope); 
+		Vector3 northSlope = Position + Vector3.forward * ChunkRadius;
+		northSlope = GetTerrainHeight(northSlope); 
+		Vector3 southSlope = Position - Vector3.forward * ChunkRadius;
+		southSlope = GetTerrainHeight(southSlope);
+
+		//Get all angles from all directions
+		float eastDeltaX = Mathf.Abs(eastSlope.x - Position.x);
+		float eastDeltaY = Mathf.Abs(eastSlope.y - Position.y);
+		float eastAngle = Mathf.Atan2(eastDeltaY, eastDeltaX);
+		float westDeltaX = Mathf.Abs(westSlope.x - Position.x);
+		float westDeltaY = Mathf.Abs(westSlope.y - Position.y);
+		float westAngle = Mathf.Atan2(westDeltaY, westDeltaX);
+	
+		float northDeltaZ = Mathf.Abs(northSlope.z - Position.z);
+		float northDeltaY = Mathf.Abs(northSlope.y - Position.y);
+		float northAngle = Mathf.Atan2(northDeltaY, northDeltaZ);
+		float southDeltaZ = Mathf.Abs(southSlope.z - Position.z);
+		float southDeltaY = Mathf.Abs(southSlope.y - Position.y);
+		float southAngle = Mathf.Atan2(southDeltaY, southDeltaZ);
+
+		//Get the steepest angle
+		currentMaxSlopeAngle = eastAngle;
+		currentMaxSlopeAngle = currentMaxSlopeAngle > westAngle ? currentMaxSlopeAngle : westAngle;
+		currentMaxSlopeAngle = currentMaxSlopeAngle > northAngle ? currentMaxSlopeAngle : northAngle;
+		currentMaxSlopeAngle = currentMaxSlopeAngle > southAngle ? currentMaxSlopeAngle : southAngle; 
+
+		return currentMaxSlopeAngle * Mathf.Rad2Deg; 
 	}
 
 	private void OnDrawGizmos()
--- The Big Project (3D)/Assets/TerrainGen/Scripts/HouseGenerator2.cs
+++ The Big Project (3D)/Assets/TerrainGen/Scripts/HouseGenerator2.cs
@@ -3,32 +3,38 @@ using System.Collections.Generic;
 
 public class HouseGenerator2 : MonoBehaviour
 {
-	public float ChunkWidth = 10;
-	public float ChunkHeight = 10;
-	public float HousePlotRadius = 1;
-	public int AmountOfHouses = 10; 
+	[Header("Settings")]
+	[SerializeField]
+	private float HousePlotRadius = 1;
+	[SerializeField]
+	private int AmountOfHouses = 10; 
 
+	[Header("Components")]
+	[SerializeField]
+	private GameObject[] HousePrefabs;
+	[SerializeField]
+	private Transform WaterSurfaceTransform; 
+
+	private Vector3 ChunkPosition; 
+	private Vector2 ChunkSize; 
+
+	private HousePlot[,] HousePlots; 
 	private int AmountOfHousesHor;
 	private int AmountOfHousesVer;
 
-	[SerializeField]
-	private GameObject[] HousePrefabs;
-
-	//List<HousePlot> HousePlots = new List<HousePlot>();
-	HousePlot[,] HousePlots; 
-
-	private void Start()
+	public void GenerateHouses(Vector3 _chunkPosition, float ChunkRadius)
 	{
-		ChunkWidth = GetComponent<Renderer>().bounds.size.x;
-		ChunkHeight = GetComponent<Renderer>().bounds.size.z;
-		GenerateHousePlots();
-		BuildHouses(); 
+		ChunkSize.x = ChunkRadius * 2;
+		ChunkSize.y = ChunkRadius * 2;
+		ChunkPosition = _chunkPosition; 
+
+		GenerateHousePlots(); 
 	}
 
-	void GenerateHousePlots()
+	private void GenerateHousePlots()
 	{
-		AmountOfHousesHor = Mathf.RoundToInt(ChunkWidth / (HousePlotRadius * 2));
-		AmountOfHousesVer = Mathf.RoundToInt(ChunkHeight / (HousePlotRadius * 2));
+		AmountOfHousesHor = Mathf.RoundToInt(ChunkSize.x / (HousePlotRadius * 2));
+		AmountOfHousesVer = Mathf.RoundToInt(ChunkSize.y / (HousePlotRadius * 2));
 		HousePlots = new HousePlot[AmountOfHousesHor, AmountOfHousesVer];
 		
 		//Makes sure there aren't more houses than plots or less houses than horizontal rows
@@ -37,7 +43,7 @@ public class HouseGenerator2 : MonoBehaviour
 		else if (AmountOfHouses > AmountOfHousesHor * AmountOfHousesVer)
 			AmountOfHouses = AmountOfHousesHor * AmountOfHousesVer; 
 
-		Vector3 startPos = new Vector3(transform.position.x - ChunkWidth / 2 + HousePlotRadius, transform.position.y, transform.position.z + ChunkHeight / 2 - HousePlotRadius);
+		Vector3 startPos = new Vector3(ChunkPosition.x - ChunkSize.x / 2 + HousePlotRadius, ChunkPosition.y, ChunkPosition.z + ChunkSize.y / 2 - HousePlotRadius);
 		Vector3 currentPos = startPos;
 		int amountOfHousesPerRow = Mathf.RoundToInt(AmountOfHouses / AmountOfHousesVer);
 
@@ -63,25 +69,75 @@ public class HouseGenerator2 : MonoBehaviour
 			currentPos.z -= HousePlotRadius * 2; 
 			currentPos.x = startPos.x; 
 		}
+
+		BuildHouses(); 
 	}
 
-	void BuildHouses()
+	private void BuildHouses()
 	{
-		foreach(HousePlot housePlot in HousePlots)
+		for (int y = 0; y < HousePlots.GetLength(1); y++)
+		{
+			for (int x = 0; x < HousePlots.GetLength(0); x++)
+			{
+				if (!HousePlots[x, y].Open)
+					continue;
+
+				int randAngle = Random.Range(0, 360);				
+				Quaternion rot = GetTerrainSlope(HousePlots[x, y].Position);		
+				HousePlots[x, y].Position = GetTerrainHeight(HousePlots[x, y].Position);
+				if (HousePlots[x, y].Position.y < WaterSurfaceTransform.position.y)
+					continue; 
+				
+				GameObject house = Instantiate(HousePrefabs[0], HousePlots[x, y].Position, rot);
+				house.GetComponent<Transform>().RotateAround(house.transform.position, house.transform.up, randAngle); 
+			}
+		}
+
+		/*foreach(HousePlot housePlot in HousePlots)
 		{
 			if (!housePlot.Open)
-				continue; 
+				continue;
 
 			int randAngle = Random.Range(0, 360);
-			Quaternion rot = Quaternion.Euler(0, randAngle, 0); 
+			Quaternion rot = Quaternion.Euler(0, randAngle, 0);
+			Vector3 position = GetTerrainHeight(housePlot.Position);
 
-			Instantiate(HousePrefabs[0], housePlot.Position, rot); 
+			Instantiate(HousePrefabs[0], housePlot.Position, rot);
+		}*/
+	}
+
+	private Vector3 GetTerrainHeight(Vector3 position)
+	{
+		Vector3 newPosition = position;
+
+		RaycastHit hit;
+		if (Physics.Raycast(new Vector3(position.x, 1000, position.z), -Vector3.up, out hit, Mathf.Infinity))
+		{
+			newPosition.y = hit.point.y;
 		}
+
+		return newPosition;
+	}
+
+	private Quaternion GetTerrainSlope(Vector3 position)
+	{
+		Quaternion newRotation = new Quaternion(); 
+
+		RaycastHit hit;
+		if (Physics.Raycast(new Vector3(position.x, 1000, position.z), -Vector3.up, out hit, Mathf.Infinity))
+		{
+			newRotation = Quaternion.FromToRotation(Vector3.up, hit.normal);			
+		}
+
+		return newRotation; 
 	}
 
 	private void OnDrawGizmos()
 	{
 		if (!Application.isPlaying)
+			return;
+
+		if (HousePlots.GetLength(1) == 0)
 			return; 
 
 		foreach (HousePlot house in HousePlots)
